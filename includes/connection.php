0b2e2e6e351201480c300b67910a27b210e6d5b0
Janarthanan <93751501+blacklovertech@users.noreply.github.com>
2023-05-18 22:21:56 +0530

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect("containers-us-west-46.railway.app","root","AimAqsgem9eqHOpbhUvN","railway" ) or die ("error" . mysqli_error($conn));
+$conn = mysqli_connect("containers-us-west-46.railway.app:7990","root","AimAqsgem9eqHOpbhUvN","railway" ) or die ("error" . mysqli_error($conn));
 ?>
