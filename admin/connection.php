ed6ab13585dc66f8be7368a7fa458120a4937ac0
Janarthanan <93751501+blacklovertech@users.noreply.github.com>
2023-05-24 14:32:50 +0100

    Update connection.php

--- admin/connection.php
+++ admin/connection.php
@@ -1,4 +1,18 @@
 
 
 <?php
-$conn = mysqli_connect("localhost","root","","notes" ) or die ("error" . mysqli_error($conn));
+
+$dbhost_name = "containers-us-west-52.railway.app:6490";
+$database = "railway";// database name
+$username = "root"; // user name
+$password = "HHLmF90ClybDFOSeZwbH"; // password 
+
+//////// Do not Edit below /////////
+try {
+$dbo = new PDO('mysql:host=localhost;dbname='.$database, $username, $password);
+} catch (PDOException $e) {
+print "Error!: " . $e->getMessage() . "<br/>";
+die();
+}
+
+$conn = mysqli_connect("$dbhost_name","$username","$password","$database" ) or die ("error" . mysqli_error($conn));
